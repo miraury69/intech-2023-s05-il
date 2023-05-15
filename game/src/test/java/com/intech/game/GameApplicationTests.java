02640c4dce469456e97f42c70d5b76d6dda77846
romainsessa <romainsessa@gmail.com>
2023-05-15 15:35:42 +0200

    profile test

--- game/src/test/java/com/intech/game/GameApplicationTests.java
+++ game/src/test/java/com/intech/game/GameApplicationTests.java
@@ -2,6 +2,7 @@ package com.intech.game;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 class GameApplicationTests {
---
+++ game/src/test/java/com/intech/game/controller/PlayerControllerFunctionnalTest.java
@@ -0,0 +1,50 @@
+package com.intech.game.controller;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.ActiveProfiles;
+
+import com.intech.game.model.Player;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
+public class PlayerControllerFunctionnalTest {
+
+	@Autowired
+	private TestRestTemplate restTemplate;	
+	
+	@Test
+	@Timeout(unit = TimeUnit.MINUTES, value = 1)
+	public void getPlayers() {
+		
+		Player[] players = restTemplate.getForObject("/player", Player[].class);
+		
+		assertEquals(1, players[0].getPlayerId());
+		assertEquals("test1", players[0].getUsername());
+		assertEquals(10, players[0].getLevel());
+		
+		assertEquals(2, players[1].getPlayerId());
+		assertEquals("test2", players[1].getUsername());
+		assertEquals(4, players[1].getLevel());		
+	}
+	
+	@Test
+	@Timeout(unit = TimeUnit.MINUTES, value=1)
+	public void getPlayer() {
+		Player player = restTemplate.getForObject("/player/1", Player.class);
+		
+		assertEquals(1, player.getPlayerId());
+		assertEquals("test1", player.getUsername());
+		assertEquals(10, player.getLevel());
+		assertEquals(2, player.getRewards().size());
+	}	
+
+}
